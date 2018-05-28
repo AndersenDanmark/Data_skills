65601535c8dfa01749aff8373c24159550932834
Andrew Yan <38387309+AndersenDanmark@users.noreply.github.com>
2018-05-28 10:46:06 -0600

    jobdescription added

--- csvTest.py
+++ csvTest.py
@@ -3,7 +3,7 @@ import csv
 with open('C:/Users/Andrew Yan/Documents/GitHub/data_skills/persons.csv', 'w') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['Name', 'Profession'])
+    filewriter.writerow(['jobTitle', 'companyName','location','jobSummary'])
     filewriter.writerow(['Derek', 'Software Developer'])
     filewriter.writerow(['Steve', 'Software Developer'])
     filewriter.writerow(['Paul', 'Manager'])
